7f7b68f2b8931789b0eae313938c7b1331e82134
marunemo <chys3697@gmail.com>
2022-03-21 18:14:17 +0900

    update: robots.txt checking

--- Example/Python/Practice/Practice1.py
+++ Example/Python/Practice/Practice1.py
@@ -9,7 +9,7 @@ robotsTxt = requests.get(targetURL + "/robots.txt")
 
 # 접근할 수 없을 시 오류 출력
 if robotsTxt.status_code != 200:
-    raise Exception(robotsTxt.status_code)
+    raise Exception("HTTP 상태 코드: " + str(robotsTxt.status_code))
 
 # 크롤링할 수 있는 지 탐색
 searchAble = {"User-agent" : [], "Allow" : [], "Disallow" : []}
@@ -19,4 +19,8 @@ for line in robotsTxt.text.replace("\r", "").split("\n"):
         searchAble[key].extend(value.split())
     else:
         searchAble[key] = [value]
-print(searchAble)
+
+seed = []
+for url in seed:
+    if "*" in searchAble["User-agent"] and url.replace(targetURL, "") in searchAble["Disallow"]:
+        raise Exception(url + "에 접근이 제한되어있습니다.")
